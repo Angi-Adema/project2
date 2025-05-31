01ff504f35836ceaa0653f110d95781f38de5bee
Angi-Adema <dr.adema@gmail.com>
2025-05-31 12:15:54 -0600

    added AccountController, MessageController and SocialMediaController to the controller folder, added Account and Message to the entity folder, added AccountRepository and MessageRepository to the repository folder and AccountService and MessageService to the service folder. Updated the logic in AccountController to handle the register functionality as well as updated AccountService with the same. Added logic to AccountRepository interface to handle the registration using JPA.

---
+++ src/main/java/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import entity.Account;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Account findByUsername(String username);
+} 
+
+
